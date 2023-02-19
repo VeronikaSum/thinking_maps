066e9041c5299236a96bb556b2f28c787828c9b1
Veronika Sumilova <veronika.sumilova@Veronikas-MBP.lan>
2023-02-19 12:30:11 +0200

    server side fixes before deploy

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,25 +1,22 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ThinkingMapModule } from './thinking-map/thinking-map.module';
 import { ImageModule } from './image/image.module';
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiModule } from './api/api.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    ThinkingMapModule, ImageModule,
+    ThinkingMapModule,
+    ImageModule,
     TypeOrmModule.forRoot({
-      type: "sqlite",
-      database: "thinkingMapDB.db",
-      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      type: 'sqlite',
+      database: 'thinkingMapDB.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     ApiModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
